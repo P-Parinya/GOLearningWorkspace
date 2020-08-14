f8561214b1320090cbd06d2d7ede010873fd62ca
P-Parinya <ppinggii@gmail.com>
2020-08-14 23:45:17 +0700

    commit day2 assignment

---
+++ wordCount.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	counter := map[string]int{}
+
+	//break long string into each word.
+	wordList := strings.Split(s," ")
+
+	//loop check word by word.
+	for _,word:= range wordList{
+		_ , ok := counter[word] //check that word existed.
+		if ok {
+			//if which word was existed, increase count number by 1.
+			counter[word] = counter[word]+1
+		}else{
+			//if which word was not existed, initial count number.
+			counter[word] = 1
+		}
+	}
+	
+	return counter
+}
+
+func main() {
+	s := "If it looks like a duck, swims like a duck, and quacks like a duck, then it probably is a duck"
+	w := WordCount(s)
+	fmt.Println(w)
+}
